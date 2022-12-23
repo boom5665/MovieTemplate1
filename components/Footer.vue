1c36a14a5b181c4fbc09a403fdb453447fbcf0a4
pcnadev <boombs5665@gmail.com>
2022-12-23 18:21:09 +0700

    banner

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <div id="footer">
         <b-container class="h-100">
             <div class="d-flex align-items-center justify-content-center h-100">
-   <nuxt-link to="/" style="text-align: center;">
+                <nuxt-link to="/" style="text-align: center">
                     <nuxt-img format="webp" :src="logo" width="500" height="100" :alt="SEOTitle" style="width: 60%; height: 60%" />
                 </nuxt-link>
                 <!-- <div class="back-to-top" @click="scrollToTop()"><b-icon-arrow-up class="icon" /></div> -->
